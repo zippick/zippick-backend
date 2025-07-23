a8ffe2612448da53317fac45e746cbd9c5540122
price126 <hwjun12@gmail.com>
2025-07-23 15:04:28 +0900

    [FIX] aws관련 cicd 수정

--- zippick/src/main/java/zippick/config/S3Config.java
+++ zippick/src/main/java/zippick/config/S3Config.java
@@ -2,7 +2,6 @@ package zippick.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
