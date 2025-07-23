2ff18198d20ae9d80fe8fb1393a4f34796938adc
price126 <hwjun12@gmail.com>
2025-07-23 11:35:12 +0900

    [TEST] S3 업로드, 다운로드 테스트

---
+++ zippick/src/main/java/zippick/config/S3Config.java
@@ -0,0 +1,30 @@
+package zippick.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
+public class S3Config {
+
+  // 기본
+  @Bean
+  public S3Client s3Client() {
+    return S3Client.builder()
+        .region(Region.AP_NORTHEAST_2) // 서울 리전
+        .build();
+  }
+
+  // 로컬
+  /*@Bean
+  public S3Client s3Client() {
+    return S3Client.builder()
+        .region(Region.AP_NORTHEAST_2)
+        .credentialsProvider(ProfileCredentialsProvider.create("zippick")) // 중요!!
+        .build();
+  }*/
+
+}
+
