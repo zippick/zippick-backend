ead735e985c379da60d6c9b212a96bf8973a543a
price126 <hwjun12@gmail.com>
2025-07-25 17:20:25 +0900

    [FEAT] 스웨거 인증 추가

--- zippick/src/main/java/zippick/global/config/OpenApiConfig.java
+++ zippick/src/main/java/zippick/global/config/OpenApiConfig.java
@@ -1,9 +1,13 @@
 package zippick.global.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import java.util.List;
 
 @Configuration
@@ -11,11 +15,25 @@ public class OpenApiConfig {
 
   @Bean
   public OpenAPI customOpenAPI() {
+    // 서버 설정
     Server server = new Server();
     //server.setUrl("https://zippick.n-e.kr");
     server.setUrl("http://localhost:8080");
-    server.setDescription("Production");
+    server.setDescription("Zippick Server");
 
-    return new OpenAPI().servers(List.of(server));
+    // Bearer JWT
+    SecurityScheme bearerAuthScheme = new SecurityScheme()
+            .type(SecurityScheme.Type.HTTP)
+            .scheme("bearer")
+            .bearerFormat("JWT");
+
+    // 전체 API에 Auth 추가
+    SecurityRequirement securityRequirement = new SecurityRequirement()
+            .addList("bearerAuth");
+
+    return new OpenAPI()
+            .servers(List.of(server))
+            .components(new Components().addSecuritySchemes("bearerAuth", bearerAuthScheme))
+            .addSecurityItem(securityRequirement);
   }
 }
