3595bbb90a46b39433f0de002e5a4bf08e68e394
price126 <hwjun12@gmail.com>
2025-07-26 23:48:05 +0900

    [REFACTOR] 로컬환경으로 값 재설정

--- zippick/src/main/java/zippick/global/config/S3Config.java
+++ zippick/src/main/java/zippick/global/config/S3Config.java
@@ -9,22 +9,22 @@ import software.amazon.awssdk.services.s3.S3Client;
 @Configuration
 public class S3Config {
 
-  // 기본
+  /*// 기본
   @Bean
   public S3Client s3Client() {
     return S3Client.builder()
         .region(Region.AP_NORTHEAST_2) // 서울 리전
         .build();
-  }
+  }*/
 
   // 로컬
-  /*@Bean
+  @Bean
   public S3Client s3Client() {
     return S3Client.builder()
             .region(Region.AP_NORTHEAST_2)
             .credentialsProvider(ProfileCredentialsProvider.create("zippick"))
             .build();
-  }*/
+  }
 
 }
 
